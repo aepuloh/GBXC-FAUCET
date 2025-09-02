7c4dce1375b9fe36615289cc1b22925127b508bf
aepuloh <57800632+aepuloh@users.noreply.github.com>
2025-09-02 20:20:17 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Config jaringan & token
 const CONFIG = {
   network: {
-    chainId: "0x38", // Hex string, BSC Mainnet
+    chainId: "0x38", // BSC Mainnet
     chainName: "BNB Smart Chain",
     rpcUrls: ["https://bsc-dataseed.binance.org/"],
     blockExplorerUrls: ["https://bscscan.com/"],
@@ -15,6 +15,6 @@ const CONFIG = {
     name: "KenariCoin",
     symbol: "KNC",
     decimals: 18,
-    contractAddress: "0x0000000000000000000000000000000000000000" // ganti address token asli
+    contractAddress: "0x1390f63AF92448c46368443496a2bfc1469558de" // ganti dgn address token asli
   }
 };
