c41b2cb47f7660fcdb7627a7eaf3614f33dc2aad
aepuloh <57800632+aepuloh@users.noreply.github.com>
2025-09-03 20:33:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -193,5 +193,10 @@ const CONFIG = {
         "type": "function"
       }
     ]
+  },
+  dex: {
+    routerAddress: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // PancakeSwap V2 Router mainnet
+    factoryAddress: "0xca143ce32fe78f1f7019d7d551a6402fc5350c73", // PancakeSwap Factory mainnet
+    wbnb: "0xBB4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c" // WBNB mainnet
   }
 };
